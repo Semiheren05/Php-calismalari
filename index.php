8131f89ce0e69d54e29db5c8657964c6bb16093e
Semiheren05 <semiherenn05@gmail.com>
2025-04-28 08:38:14 +0300

    Create index.php
    php derslerinin başında phpnin html dili ile nasıl kullanıldığını gösterdiğimiz kod

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP VE HTML'in birlikte çalışması</h1>
+    <p><?php echo " Bu içerik Php tarafından oluşturuldu"; ?> </p>
+    <?php
+    echo "bu bir php kodudur";
+    ?>
+</body>
+</html>
