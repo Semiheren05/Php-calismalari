d69f58008cc3a49bc3e3e55f4b919f70a5f04ecc
Semiheren05 <semiherenn05@gmail.com>
2025-04-28 09:00:57 +0300

    Create degiskenisimlendirme.php

---
+++ degiskenisimlendirme.php
@@ -0,0 +1,15 @@
+<?php
+/*
+$ simgesiyle başlamalı
+- PHP'de her değişken ismi $ simgesi ile başlamalıdır
+- $ simgesi değişkenin bir php değişkeni olduğunu ifade eder.
+- Değişken isimleri sayılarla başlamasına izin vermez 
+- Değiken isimleri boşluk , nokta tire gibi özel karakterler içemerez
+- Yalnızca harfler, rakamlar ve alt çizgi kullanılabilir
+- Php'de değişken isimleri büyük/küçük harf duyarlıdır
+- Php'de önceden tanımlanmış anahtar kelimeler değişken adı olarak kullanılamaz
+
+*/
+$isim = "Ahmet"; // Doğru
+$_isim = "mehmet";
+$isim_soyisim ="Doğru"; ?>
