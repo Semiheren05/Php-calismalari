42e3a347c0ba8adcaee2e1691bc9bc5a742bff2c
Semiheren05 <semiherenn05@gmail.com>
2025-04-28 11:01:45 +0300

    Create degiskenturleri.php

---
+++ degiskenturleri.php
@@ -0,0 +1,37 @@
+<?php
+/* 
+Değişken türleri
+
+String (Dize)
+Metinsel verileri temsil eder. tek veya çift rınak içinde tanımlanabilir
+örnek: */
+$isim = " ahmet";
+$mesaj='merhaba, PHP öğreniyorum';
+/* 
+Integer ( tam sayı)
+negatif veya pozitif tam sayıları ifade eder.
+örnek :
+*/
+$yas = 25;
+$negatifSayi = -10; 
+/* float (ondalıklı sayı)
+Ondalıklı sayıları temsil eder
+örnek :
+*/
+$pi = 3.14;
+$negatifOndalik = -0.99;
+/*
+Boolean (mantıksal)
+True veya False değerlerini alır
+örnek :
+*/
+$aktif = true;
+$girisyapildi = false;
+/*
+Array (Dizi)
+Birden fazla değeri tek bir değişkende saklamaya yarar
+örnek:
+*/
+$renkler = array("kırmızı","yeşil","mavi");
+$yaslar = [25,30,35];
+?>
