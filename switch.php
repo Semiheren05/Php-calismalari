23e7c68ab162bc59d15afe3deede6954b51dce18
Semiheren05 <semiherenn05@gmail.com>
2025-07-25 14:14:09 +0300

    Create switch.php
    Switch yapılarını öğrendiğim kod dizisi

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+$gun="Perşembe";
+
+switch ($gun) {
+      case 'Cumartesi':
+            echo "Bugün cumartesi";
+            break;
+      
+      case "Pazartesi":
+            echo " Bugün pazartesi";
+            break;
+            default:
+            echo"Günler eşleşmedi";
+}
