d540d578cd00c39ca2b7b000df7da5e05a1dd383
Semiheren05 <semiherenn05@gmail.com>
2025-07-28 11:36:36 +0300

    Update function.php

--- function.php
+++ function.php
@@ -22,3 +22,40 @@ function Selamver($isim="Misafir"){
 }
 $isim= isset($_GET["İSİM"])? $_GET["isim"]:null;
 Selamver($isim);
+<?php
+//////////////////////////////////
+
+$kategoriler = [
+      "Elektronik"=>[
+            "Bilgisayar"=>[
+            "Dizüstü",
+            "Masaüstü",
+      ],
+      "Telefon"=>[
+            "Akıllı Telefon",
+            "Sabit Telefon",
+      ],
+],
+"Ev ve Yaşam" =>[
+      "Mobilya" =>[
+            "Koltuk",
+            "Masa",
+      ],
+      "Mutfak" =>[
+            "Tencere",
+            "Tabak",
+      ],
+],
+];
+Function kategorileriGoster($kategoriler,$seviye= 0)
+{
+      foreach($kategoriler as $kategori => $altKategori){
+            if(is_array($altKategori)){
+                  echo str_repeat("-",$seviye * 2). "$kategori<br> ";
+                  kategorileriGoster($altKategori,$seviye + 1);
+            } else{
+                  echo str_repeat("-",$seviye * 2). "$altKategori<br>";
+            }
+      }
+}
+kategorileriGoster($kategoriler);
